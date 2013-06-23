207bf1f40dc9e40015062c3460de5f5bb19baa40
David Binetti <dbinetti@gmail.com>
2013-06-22 22:29:34 -0700

    Adding Heroku access patterns to prod settings
    Added the configs, but forg

--- project/project/settings/prod.py
+++ project/project/settings/prod.py
@@ -3,6 +3,11 @@ from .base import *
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = get_env_variable("AWS_STORAGE_BUCKET_NAME")
+
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
