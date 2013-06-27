fc3b08dc80e129a9527ff5356e29819314eca12c
David Binetti <dbinetti@gmail.com>
2013-06-27 11:11:10 -0700

    Remove Noncense from django project

--- project/apps/noncense/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class AuthRequestForm(forms.Form):
-    mobile = forms.CharField(max_length=12)
-
-
-class AuthResponseForm(forms.Form):
-    code = forms.CharField(max_length=4)
--- project/apps/noncense/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('apps.noncense.views',
-    # url(r'api/(?P<mobile>\d{10})/$', 'logmein', name='logmein'),
-)
