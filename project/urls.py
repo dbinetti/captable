2c486d8a8109e2ebb2e7ce718641de7bc5c61c39
David Binetti <dbinetti@gmail.com>
2013-06-27 12:33:47 -0700

    Use stock django auth

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^', include('apps.captable.urls')),
-    url(r'^', include('apps.noncense.urls'))
 )
-urlpatterns += staticfiles_urlpatterns()
