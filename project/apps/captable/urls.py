4834f81f4c31b1131d8dd72e9652663c9db42136
David Binetti <dbinetti@gmail.com>
2013-06-27 07:27:07 -0700

    Changs logout to logout_then_login

--- project/apps/captable/urls.py
+++ project/apps/captable/urls.py
@@ -33,5 +33,5 @@ urlpatterns = patterns(
 
 urlpatterns += patterns('',
     url(r'login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 )
