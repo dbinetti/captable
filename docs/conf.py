bf58605d741221f19b15ad06f3450a3ed2633241
David Binetti <dbinetti@gmail.com>
2013-08-18 09:22:00 -0700

    Adding bootstrap theme to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -91,15 +91,19 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx-bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
-
+html_theme_options = {
+    'github_user': 'dbinetti',
+    'github_repo': 'captable',
+    'twitter_username': 'dbinetti',
+    'home_url': 'http://captable.herokuapp.com',
+}
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
